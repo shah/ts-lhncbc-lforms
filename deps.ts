9f48c959257a8a64732a34ea1e98a27037acdc48
Shahid N. Shah <shahid@shah.org>
2020-09-27 17:25:06 -0400

    fix: fn messes up provenance output

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export * as fs from "https://deno.land/std@v0.70.0/fs/mod.ts";
 export * as path from "https://deno.land/std@v0.70.0/path/mod.ts";
-export * as govnDataCLI from "https://denopkg.com/gov-suite/governed-structured-data@v0.5.10/cli.ts";
-export * as govnData from "https://denopkg.com/gov-suite/governed-structured-data@v0.5.10/mod.ts";
+export * as govnDataCLI from "https://denopkg.com/gov-suite/governed-structured-data@v0.5.11/cli.ts";
+export * as govnData from "https://denopkg.com/gov-suite/governed-structured-data@v0.5.11/mod.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
