cc66a90228ecc999ffe6f96802cae1220be20b6a
Shahid N. Shah <shahid@shah.org>
2020-10-15 16:19:29 -0400

    fix: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export * as fs from "https://deno.land/std@v0.74.0/fs/mod.ts";
 export * as path from "https://deno.land/std@v0.74.0/path/mod.ts";
-export * as govnDataCLI from "https://denopkg.com/gov-suite/governed-structured-data@v0.6.1/cli.ts";
-export * as govnData from "https://denopkg.com/gov-suite/governed-structured-data@v0.6.1/mod.ts";
+export * as govnDataCLI from "https://denopkg.com/gov-suite/governed-structured-data@v0.7.0/cli.ts";
+export * as govnData from "https://denopkg.com/gov-suite/governed-structured-data@v0.7.0/mod.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
-export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.6.6/mod.ts";
+export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.6.7/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.2.4/mod.ts";
