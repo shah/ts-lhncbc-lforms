fe3202ada7337dc1d0a368333dd6204af89bcb2a
Shahid N. Shah <shahid@shah.org>
2020-10-19 10:07:56 -0400

    fix: remove Deno std@v from dependencies

--- deps-test.ts
+++ deps-test.ts
@@ -1,2 +1,2 @@
 // Dependencies required during engineering/testing only (not in production)
-export * as testingAsserts from "https://deno.land/std@v0.74.0/testing/asserts.ts";
+export * as testingAsserts from "https://deno.land/std@0.74.0/testing/asserts.ts";
