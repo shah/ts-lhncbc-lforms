d69283bc7d4a167de213db772fb574d66bf3a9fd
geo_vl <geo@citrusinformatics.com>
2021-04-21 15:07:06 +0530

    update: docopt for deno 1.9.0

--- deps-test.ts
+++ deps-test.ts
@@ -1,2 +1,2 @@
 // Dependencies required during engineering/testing only (not in production)
-export * as testingAsserts from "https://deno.land/std@0.80.0/testing/asserts.ts";
+export * as testingAsserts from "https://deno.land/std@0.94.0/testing/asserts.ts";
--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-export * as colors from "https://deno.land/std@0.80.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.80.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.80.0/path/mod.ts";
-export * as govnDataCLI from "https://denopkg.com/gov-suite/governed-structured-data@v0.7.9/cli.ts";
-export * as govnData from "https://denopkg.com/gov-suite/governed-structured-data@v0.7.9/mod.ts";
+export * as colors from "https://deno.land/std@0.94.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.94.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.94.0/path/mod.ts";
+export * as govnDataCLI from "https://denopkg.com/gov-suite/governed-structured-data@v0.7.10/cli.ts";
+export * as govnData from "https://denopkg.com/gov-suite/governed-structured-data@v0.7.10/mod.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
-export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.9/mod.ts";
+export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.9.1/mod.ts";
 export * as jsonMutator from "https://denopkg.com/shah/ts-json-mutator@v0.1.2/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.1/mod.ts";
 export * as jsonPatch from "https://denopkg.com/Starcounter-Jack/JSON-Patch@3.0.0-1/index.mjs";
